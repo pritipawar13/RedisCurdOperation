9b8e557035e0386547307f686b2edb6d418697c1
pritipawar13 <pritipawar131999@gmail.com>
2021-09-03 13:48:11 +0530

    query change

--- Todo/Repository/User-Repository.js
+++ Todo/Repository/User-Repository.js
@@ -1,20 +1,22 @@
 const router = require('express').Router();
 const TodoUser = require('../Model/User');
 
-const CheckUser = async(email) => {
-   return await TodoUser.findOne({ Email: email })  
+const CheckUser = async(UserData) => {
+   return await TodoUser.where('Email','==','UserData.email')  
 }
 
-const GetAllUser= async ()=>{
+const GetAllUser = async ()=>{
      return await TodoUser.find({});
-  }
+}
 
 const GetPerticularUserDetails = async (userid) =>{
-   return await TodoUser.findById({ _id : userid})
+  return await TodoUser.where('_id', '==', userid)
 }
+
 const GetUserByUsingToken = async(token) =>{
     return await TodoUser.find({ Email :token.Email})
 }
+
 module.exports={
     GetAllUser,
     CheckUser,
