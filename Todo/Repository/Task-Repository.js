f4b58fccfdc62ac1c081d72178001fab81ced749
pritipawar13 <pritipawar131999@gmail.com>
2021-09-03 13:47:33 +0530

    query change

--- Todo/Repository/Task-Repository.js
+++ Todo/Repository/Task-Repository.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const UserTask = require('../Model/Tasks');
 
-const ShowAllTasks = async (username) => {
-    return UserTask.find({Username : username})
+const ShowAllTasks = async (Username) => {
+    return UserTask.where('UserName','==','Username.username')
 }
-const MarkCompletedTask = async (taskid,completed) => {
-   return await UserTask.findByIdAndUpdate({ _id : taskid },{ $set :{ Completed :completed , CompletedTime: new Date()}})
+const MarkCompletedTask = async (id,TaskData) => {
+   return await UserTask.findByIdAndUpdate({ _id : id.taskid },{ $set :{ Completed :TaskData.completed , CompletedTime: new Date()}})
 }
 
 const ShowAllCompletedTask = async () =>{
@@ -20,8 +20,8 @@ const RemoveAllCompletedTask = async () => {
    return await UserTask.findOneAndDelete({ Completed:true})
 }
 
-const UserAndTask = async (userid) =>{
-    return await UserTask.find({userid: userid}).populate({ path: 'User', select: 'Firstname Lastname Email Type' })
+const UserAndTask = async (id) =>{
+    return await UserTask.find({userid: id.userid}).populate({ path: 'User', select: 'Firstname Lastname Email Type' })
 }
 module.exports = {
     ShowAllTasks,
