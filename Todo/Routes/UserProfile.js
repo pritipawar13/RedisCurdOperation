cdbbd666e64e191083e29aae7fc29cb824b4bc5d
pritipawar13 <pritipawar131999@gmail.com>
2021-09-07 16:28:41 +0530

    User Profile Routes

--- Todo/Routes/UserProfile.js
+++ Todo/Routes/UserProfile.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const UserProfileController = require('../Controller/Userprofile-Controller')
+const UserProfileController = require('../Controller/Userprofile-Controller');
 
 
 router.post('/add-details-in-profile/:userid' ,UserProfileController.upload , UserProfileController.AddUserProfile );
