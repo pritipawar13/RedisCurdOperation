df7d1dca87ac6da91c904782fc4df859d372a5c4
Priti Rajendra Pawar <73785568+pritipawar13@users.noreply.github.com>
2021-09-09 11:46:23 +0530

    Delete Todo directory

--- Todo/Routes/UserProfile.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const UserProfileController = require('../Controller/Userprofile-Controller');
-
-
-router.post('/add-details-in-profile/:userid' ,UserProfileController.upload , UserProfileController.AddUserProfile );
-
-
-module.exports = router;
